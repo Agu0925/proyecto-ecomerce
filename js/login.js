6086b20a62aa387fa1ac730e693464e2d71326d9
Agustin A <agustin.ahlers@hotmail.com>
2022-08-15 20:15:01 -0300

    'OAuth-Listo-Para-Implementar_En-BackEnd'

--- js/login.js
+++ js/login.js
@@ -153,13 +153,6 @@ btnregistrarse.addEventListener("click",function(event){
         alert('Se Registro Correctamente!')
     }
 })
-// login con Google
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+
 
 //-------------------------------------------------------------------------------------------
