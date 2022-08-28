c5d8cbf1d9d1351298e9216f153daeff08c8560b
Agustin A <agustin.ahlers@hotmail.com>
2022-08-28 03:19:09 -0300

    'Entregable2-intentos-ordenar-y-buscador'

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,6 +3,7 @@ let Nusuario = document.getElementsByClassName('Nusuario');
 let arrayCuentas = localStorage.getItem('Cuentas');
 cuentas = JSON.parse(arrayCuentas);
 // ----------------------------------------------------
+//Recorro todas las clases Nusuario y las modifico
 for (i = 0; i < Nusuario.length; i++){
 Nusuario[i].innerHTML = "<a class='nav-link' href='my-profile.html'> " + cuentas.nombre + " </a>";
 }
