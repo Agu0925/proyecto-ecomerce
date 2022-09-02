f7a3651fc1ce4ca94eccc33978261d3a9c09add4
Agustin A <agustin.ahlers@hotmail.com>
2022-09-02 12:49:34 -0300

    'Buscador-y-Filtros-Terminados'

--- js/products.js
+++ js/products.js
@@ -3,6 +3,7 @@ let minCount = undefined;
 let maxCount = undefined;
 let Pmin = undefined;
 let Pmax = undefined;
+let buscar = undefined;
 
 // funcion para tomar la id con onclick
     function setCatID(id) {
@@ -17,31 +18,35 @@ let Pmax = undefined;
             function mostrar(){
             let htmlContentToAppend = "";
         //Recorrro el array de la url y lo delcaro en una variable para poder usarlo con su respectiva categoria 
-                for(let i = 0; i < datos.products.length; i++){ 
+            for(let i = 0; i < datos.products.length; i++){ 
                     let category = datos.products[i];
         //Filtro
-                    if(parseInt(category.cost) >= minCount && parseInt(category.cost) <= maxCount || minCount == undefined && maxCount == undefined)
-        //agrego todo al html
-                    htmlContentToAppend += `
-                    <div onclick="setCatID(` + category.id + `)" class="list-group-item list-group-item-action cursor-active">
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="` + category.image + `" alt="` + category.description + `" class="img-thumbnail">
-                            </div>
-                            <div class="col">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h4 class="mb-1">` + category.name + ` - ` + category.currency + ` ` + category.cost +`</h4>
-                                    <small class="text-muted">` + category.soldCount + ` vendidos</small>
+                if(parseInt(category.cost) >= minCount && parseInt(category.cost) <= maxCount || minCount == undefined && maxCount == undefined){
+        //Filtro Buscador
+                        //toLowerCase para convertir todo a minusculas y includes para filtrar por cada string del array
+                    if (category.name.toLowerCase().includes(buscar) || buscar == undefined || category.description.toLowerCase().includes(buscar)){
+                    //agrego todo al html
+                        htmlContentToAppend += `
+                        <div onclick="setCatID(` + category.id + `)" class="list-group-item list-group-item-action cursor-active">
+                            <div class="row">
+                                <div class="col-3">
+                                    <img src="` + category.image + `" alt="` + category.description + `" class="img-thumbnail">
                                 </div>
-                                <p class="mb-1" id="des-product">` + category.description + `</p>
+                                <div class="col">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h4 class="mb-1">` + category.name + ` - ` + category.currency + ` ` + category.cost +`</h4>
+                                        <small class="text-muted">` + category.soldCount + ` vendidos</small>
+                                    </div>
+                                    <p class="mb-1" id="des-product">` + category.description + `</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    `;
-                    document.getElementById("container-products").innerHTML = htmlContentToAppend;
-                    //pongo el array en lista para ordenarlo
+                        `;
+                        document.getElementById("container-products").innerHTML = htmlContentToAppend;
+                    }
                 }
             }
+        }
         //Filtrando datos --------------------
         mostrar()
         //Boton Filtro por Precio
@@ -103,10 +108,13 @@ let Pmax = undefined;
              mostrar();
         });
         //Ordenar por Buscador
-        document.getElementById('buscador').addEventListener('keydown', function(){
+        document.getElementById('buscador').addEventListener('input', function(){
+            buscar =  this.value.toLowerCase();
+            mostrar();
         });
         document.getElementById('btn-buscar').addEventListener('click', function(){
-            
+            buscar =  document.getElementById('buscador').value.toLowerCase();
+            mostrar();
         });
     })
     
