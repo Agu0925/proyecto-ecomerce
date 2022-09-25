2ddf6a2f8a49df1c95145e51b9e3e24d450f14b5
Agustin A <agustin.ahlers@hotmail.com>
2022-09-24 23:21:22 -0300

    'E4-Pauta2-Terminada'

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,7 +3,26 @@ let Nusuario = document.getElementsByClassName('Nusuario');
 let arrayCuentas = localStorage.getItem('Cuentas');
 let cuentas = JSON.parse(arrayCuentas);
 // ----------------------------------------------------
+// Funcion Cerrar Sesion
+function cerrarS(){
+    localStorage.removeItem('logueado?');
+}
 //Recorro todas las clases Nusuario y las modifico
 for (i = 0; i < Nusuario.length; i++){
-Nusuario[i].innerHTML = "<a class='nav-link' href='my-profile.html'> " + cuentas.nombre + " </a>";
+Nusuario[i].innerHTML = `
+<div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+  <ul class="navbar-nav">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      ${cuentas.nombre}
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+        <!-- Redirigo con href y utilizo una funcion con onclick en cerrar sesion -->
+        <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
+        <li><a class='dropdown-item' href='my-profile.html'>  Mi perfil </a></li>
+        <li><a class="dropdown-item" href="index.html" onclick="cerrarS()" id='cerrar-sesion'> Cerrar sesion </a></li>
+      </ul>
+    </li>
+  </ul>
+</div>`;
 }
