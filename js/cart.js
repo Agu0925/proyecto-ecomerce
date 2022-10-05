92a0de0ef7791a4034ccc158dc414fbadaa3a65c
Agustin A <agustin.ahlers@hotmail.com>
2022-10-05 13:38:07 -0300

    'sssss'

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function mostrar() {
    fetch(CART_INFO_URL)
       .then((resp) => resp.json())
@@ -43,18 +42,3 @@ fetch(urlpaises)
       }
    })
 
-=======
-//Paises
-let urlpaises = "https://gist.githubusercontent.com/Yizack/bbfce31e0217a3689c8d961a356cb10d/raw/107e0bdf27918adea625410af0d340e8fc1cd5bf/countries.json";
-fetch(urlpaises)
-.then((resp) => resp.json())
-.then((datos) => {
-    let paises = "";
-    for (const iterator of datos.countries) {
-        paises += `
-            <option value="">${iterator.name_es}</option>
-        `
-        document.getElementById("pais").innerHTML =`<option value="" disabled selected hidden>Pais</option>` + paises
-    }
-})
->>>>>>> 6094e865e473fd10beb3604bd4aad790230bfa8d
